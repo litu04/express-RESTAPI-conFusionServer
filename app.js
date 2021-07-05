504597b3734fd965238524ba8daa99468a434d6b
litu04 <debadattakar96@gmail.com>
2021-07-05 13:44:32 +0530

    passport

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 const mongoose = require('mongoose');
 var session = require('express-session');
 var fileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -43,24 +45,22 @@ app.use(session({
   store: new fileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req,res,next){
-  console.log("session: ",req.session);
+  //console.log("session: ",req.session);
+  console.log("user: ",req.user);
 
-  if(!req.session.user){
+  if(!req.user){
         var err = new Error("You are not authenticated!");
-        err.status = 401;
-        return next(err);
+        err.status = 403;
+        next(err);
       }else{
-        if(req.session.user = 'authenticated'){
-          next(); // req will pass to the next middleware
-         } else{
-            var err = new Error("You are not authenticated!");
-            err.status = 403;
-            return next(err);
-          }
+        next();
       }
 }
 
--- models/user.js
+++ models/user.js
@@ -1,21 +1,24 @@
 const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
+    // username: {
+    //     type: String,
+    //     required: true,
+    //     unique: true
+    // },
+    // password: {
+    //     type: String,
+    //     required: true
+    // },
     admin: {
         type: Boolean,
         default: false
     }
 });
 
+userSchema.plugin(passportLocalMongoose);
 const User = mongoose.model('User',userSchema);
 
 module.exports = User;
